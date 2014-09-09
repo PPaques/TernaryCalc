512b1c813fb6ea469f05263d27fcd55a26f33be8
Pierre Paques <pierre@paques.eu.com>
2014-09-09 11:35:42 +0200

    round calc

--- Program.cs
+++ Program.cs
@@ -13,40 +13,40 @@ namespace TernaryCalc
         {
             // Step 0 : (UI) Print welcome message
             Console.WriteLine("Welcome to Ternary Calc Software");
-            Console.WriteLine("Please enter your ternary operations then press enter:");
 
-            try
+            while (true)
             {
-                // Step 1 : get the string to compute
-                string original_line = Console.ReadLine();
-                
-                // Step 2 : Convert all numbers to decimal
-                string base10_line = Ternary.ConvertCalcLineFromBase10(original_line);
-                
-                // Step 3 : Parse and execute the string
-                string base10_result = new Expression(base10_line).Evaluate().ToString();
-                //Console.WriteLine("decimal : " + base10_line + " = " + base10_result);
-                
-                // Step 4 : Convert back in base 3
-                string base3_result = Ternary.ConvertFromBase10(base10_result);
+                try
+                {
+                    // Step 1 : get the string to compute
+                    Console.WriteLine("Please enter your ternary operations then press enter:");
+                    string original_line = Console.ReadLine();
 
-                // Step 5 : show result
-                Console.WriteLine("Base 3 : " + original_line + " = " + base3_result);
-                
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Press any key to retry..");
-                Console.ReadKey();
-                /// TODO Return to state 0
-            }
+                    // Step 2 : Convert all numbers to decimal
+                    string base10_line = Ternary.ConvertCalcLineFromBase10(original_line);
 
-            Console.WriteLine("Press any key to quit the Ternary Calc");
-            Console.ReadKey();
+                    // Step 3 : Parse and execute the string
+                    string base10_result = new Expression(base10_line).Evaluate().ToString();
+                    //Console.WriteLine("decimal : " + base10_line + " = " + base10_result);
+
+                    // Step 4 : Convert back in base 3
+                    string base3_result = Ternary.ConvertFromBase10(base10_result);
+
+                    // Step 5 : show result
+                    Console.WriteLine("Base 3 : " + original_line + " = " + base3_result);
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Press any key to retry.");
+                    continue;
+                }
+
+                Console.WriteLine("Press escape to exit or any key to make another operation");
+                if (Console.ReadKey().Key == ConsoleKey.Escape)
+                    break;
+                Console.Clear();
+            }
         }
-
-       
-
-       
     }
 }
