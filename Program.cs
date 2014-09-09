bdddded510e14c3281eeb5d6356a41af08674a1b
Pierre Paques <pierre@paques.eu.com>
2014-09-09 10:13:04 +0200

    adding idea of working

--- Program.cs
+++ Program.cs
@@ -10,6 +10,14 @@ namespace TernaryCalc
     {
         static void Main(string[] args)
         {
+            // Idea 
+            
+            // Step 1 : get the string calc
+            // Step 2 : Convert all numbers to decimal
+            // Step 3 : Parse and execute the string
+            // Step 4 : Convert back in base 3
+            // Step 5 : show result
+
         }
     }
 }
